e1086954e52630a3ae00a63f5e5616c0731dfb41
anandaru <anand_aru@yahoo.com>
2022-05-26 21:40:59 +0530

    Add files via upload

---
+++ Ex1-Linear Regression/gradientDescentMulti.m
@@ -0,0 +1,52 @@
+function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENTMULTI Performs gradient descent to learn theta
+%   theta = GRADIENTDESCENTMULTI(x, y, theta, alpha, num_iters) updates theta by
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+n = length(theta); % number of features + 1
+derivatives = zeros(n, 1); % initialize fitting parameters
+J_history = zeros(num_iters, 1);
+
+cost = computeCostMulti(X, y, theta);
+fprintf('cost = %f\n', cost);
+
+for iter = 1:num_iters
+
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Perform a single gradient step on the parameter vector
+    %               theta. 
+    %
+    % Hint: While debugging, it can be useful to print out the values
+    %       of the cost function (computeCostMulti) and gradient here.
+    %
+    for j = 1:n,
+      derivatives(j) = 0 ;
+    end
+    %hypothesis
+    y_pred = X * theta;
+    
+    # partial derivative of cost function wrt theta
+    for j = 1:n,
+        for k = 1:m,
+          derivatives(j)= derivatives(j) + ((y_pred(k) - y(k)) * X(k,j))/m;
+        end 
+    end  
+    # Find better theta to get lesser cost
+    for j = 1:n,
+      theta(j) = theta(j) - (alpha * derivatives(j));
+    end 
+    cost = computeCostMulti(X, y, theta);
+    fprintf('cost = %f\n', cost);
+
+
+
+    % ============================================================
+
+    % Save the cost J in every iteration    
+    J_history(iter) = cost;
+
+end
+
+end
